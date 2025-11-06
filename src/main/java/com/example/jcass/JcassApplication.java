027641e38948bfee678e424e6587338a0e0d4713
x1y1z1 <glina41@o2.pl>
2025-11-06 20:55:35 +0100

    jcass

---
+++ src/main/java/com/example/jcass/JcassApplication.java
@@ -0,0 +1,13 @@
+package com.example.jcass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcassApplication.class, args);
+	}
+
+}
